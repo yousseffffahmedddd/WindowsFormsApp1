f414398218319b0975a12470826ca8a19a002210
Lenovo <Lenovo@DESKTOP-R2SLAOP>
2025-04-27 02:28:51 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class datagridview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.insert = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.update = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.delete = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 26;
+            this.dataGridView1.Size = new System.Drawing.Size(442, 406);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(164, 367);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(156, 23);
+            this.insert.TabIndex = 1;
+            this.insert.Text = "insert";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 337);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 24);
+            this.textBox2.TabIndex = 3;
+            // 
+            // update
+            // 
+            this.update.Location = new System.Drawing.Point(83, 376);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(75, 23);
+            this.update.TabIndex = 4;
+            this.update.Text = "update";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(339, 358);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 24);
+            this.textBox3.TabIndex = 5;
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(339, 388);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 6;
+            this.delete.Text = "delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(204, 429);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 7;
+            this.show.Text = "show";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "admin_id";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(460, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 26;
+            this.dataGridView2.Size = new System.Drawing.Size(582, 585);
+            this.dataGridView2.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(175, 90);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 24);
+            this.textBox4.TabIndex = 14;
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Location = new System.Drawing.Point(208, 70);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(38, 17);
+            this.email.TabIndex = 15;
+            this.email.Text = "email";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(175, 150);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 24);
+            this.textBox5.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "name";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(175, 206);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 24);
+            this.textBox6.TabIndex = 18;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(188, 186);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(66, 17);
+            this.password.TabIndex = 19;
+            this.password.Text = "password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "updatecertain";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "deleteceratin";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(140, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "insertceratin";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // datagridview
+            // 
+            this.AccessibleDescription = "datagridview";
+            this.AccessibleName = "datagridview";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1154, 788);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "datagridview";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class datagridview: Form
+    {
+        public datagridview()
+        {
+
+            InitializeComponent();
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-R2SLAOP\\SQLEXPRESS;Initial Catalog=Cinema_ticket_booking_system;Integrated Security=True");
+            connection.Open();
+
+
+            //SqlCommand query = new SqlCommand("insert into Admin (Admin_id,Admin_email,Admin_name,Admin_password) values('4','faaaaaagmail.com','abdo','442');", connection);
+            //query.CommandType = CommandType.Text;
+
+            //SqlDataReader reader = query.ExecuteReader();
+            //DataTable table = new DataTable();
+            //table.Columns.Add("AdminID");
+            //table.Columns.Add("AdminEMAIL");
+
+            //DataRow row;
+            //while(reader.Read())
+            //{
+            //    row = table.NewRow();
+            //    row["AdminID"] = reader["Admin_id"];
+            //    row["AdminEMAIL"] = reader["Admin_email"];
+            //    table.Rows.Add(row);
+            //}
+            //reader.Close();
+            //connection.Close();
+            //MessageBox.Show("succes");
+           // dataGridView1.DataSource = table;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-R2SLAOP\\SQLEXPRESS;Initial Catalog=Cinema_ticket_booking_system;Integrated Security=True");
+            connection.Open();
+
+            string insertstr = @"insert into Admin (Admin_id) values(@id);";
+            SqlCommand command = new SqlCommand(insertstr, connection);
+            command.CommandType = CommandType.Text;
+            SqlParameter id = new SqlParameter("id", textBox1.Text);
+            command.Parameters.Add(id);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("succses insert");
+            
+          
+        
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-R2SLAOP\\SQLEXPRESS;Initial Catalog=Cinema_ticket_booking_system;Integrated Security=True");
+            connection.Open();
+
+
+            SqlCommand query = new SqlCommand("select * from Admin", connection);
+            query.CommandType = CommandType.Text;
+
+            SqlDataReader reader = query.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Columns.Add("AdminID");
+            table.Columns.Add("AdminEMAIL");
+            table.Columns.Add("AdminNAME");
+            table.Columns.Add("AdminPAss");
+
+            DataRow row;
+            while (reader.Read())
+            {
+                row = table.NewRow();
+                row["AdminID"] = reader["Admin_id"];
+                row["AdminEMAIL"] = reader["Admin_email"];
+                row["AdminNAME"] = reader["Admin_name"];
+                row["AdminPAss"] = reader["Admin_password"];
+                table.Rows.Add(row);
+            }
+            reader.Close();
+            connection.Close();
+            MessageBox.Show("succes");
+            dataGridView2.DataSource = table;
+
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-R2SLAOP\\SQLEXPRESS;Initial Catalog=Cinema_ticket_booking_system;Integrated Security=True");
+            connection.Open();
+
+            string updatestr = @"update Admin set Admin_email=@email where Admin_id=4";
+            SqlCommand command = new SqlCommand(updatestr, connection);
+            command.CommandType = CommandType.Text;
+            SqlParameter email = new SqlParameter("email", textBox2.Text);
+            command.Parameters.Add(email);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("succses update");
+
+
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-R2SLAOP\\SQLEXPRESS;Initial Catalog=Cinema_ticket_booking_system;Integrated Security=True");
+            connection.Open();
+
+            string deletestr = @"delete Admin where Admin_name=@name";
+            SqlCommand command = new SqlCommand(deletestr, connection);
+            command.CommandType = CommandType.Text;
+            SqlParameter name = new SqlParameter("name", textBox3.Text);
+            command.Parameters.Add(name);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("succses ");
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        //insertcertain
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-R2SLAOP\\SQLEXPRESS;Initial Catalog=Cinema_ticket_booking_system;Integrated Security=True");
+            connection.Open();
+
+            string insertstr = @"insert into Admin (Admin_id,Admin_email,Admin_name,Admin_password) values(@idd,@emaill,@namee,@passw);";
+            SqlCommand command = new SqlCommand(insertstr, connection);
+            command.CommandType = CommandType.Text;
+            SqlParameter idd = new SqlParameter("idd", textBox1.Text);
+            SqlParameter emaill = new SqlParameter("emaill", textBox4.Text);
+            SqlParameter namee = new SqlParameter("namee", textBox5.Text);
+            SqlParameter passw = new SqlParameter("passw", textBox6.Text);
+            command.Parameters.Add(idd);
+            command.Parameters.Add(emaill);
+            command.Parameters.Add(namee);
+            command.Parameters.Add(passw);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("succses insert all");
+
+
+        }
+    }
+}
